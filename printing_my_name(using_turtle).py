2e991c11dfcb733e86d4c44d773a1924a9439fe4
Sagar Thapaliya <58661917+sagar2057@users.noreply.github.com>
2020-05-10 14:18:10 +0545

    Update printing_my_name(using_turtle).py

--- printing_my_name(using_turtle).py
+++ printing_my_name(using_turtle).py
@@ -7,6 +7,7 @@ turtle.bgcolor("black")
 name.pensize(10)
 name.color("white")
 
+#it displays letter 'S'
 name.penup()
 name.goto(-250, 200)
 name.pendown()
@@ -16,6 +17,7 @@ name.goto(-250, 135)
 name.goto(-250, 70)
 name.goto(-350, 70)
 
+#it displays letter 'A'
 name.penup()
 name.goto(-150, 200)
 name.pendown()
@@ -26,6 +28,7 @@ name.goto(-150, 135)
 name.goto(-150, 200)
 name.goto(-150,70)
 
+#it diplays letter 'G'
 name.penup()
 name.goto(-30, 200)
 name.pendown()
@@ -35,6 +38,7 @@ name.goto(-30, 70)
 name.goto(-30, 135)
 name.goto(-70, 135)
 
+#it diplays letter 'A'
 name.penup()
 name.goto(75, 200)
 name.pendown()
@@ -45,6 +49,7 @@ name.goto(75, 135)
 name.goto(75, 200)
 name.goto(75, 70)
 
+#it diplays letter 'R'
 name.penup()
 name.goto(100, 200)
 name.pendown()
